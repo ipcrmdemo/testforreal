94f1af56777fb5d0b066472b9c567ff118e0c1f1
Matt Cadorette <mcadorette@gmail.com>
2018-11-28 20:15:30 -0500

    Initial commit from Atomist

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import * as express from "express";
+export const app = express();
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+// tslint:disable-next-line:no-console
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
